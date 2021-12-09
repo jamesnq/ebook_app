63ced32fa4393851e71b76da22cb5692f07ef857
Huy <dodaihoc318@gmail.com>
2021-12-10 00:06:40 +0700

    file

---
+++ app/src/main/java/com/lttbdd/ebook_app/Fragment2.java
@@ -0,0 +1,21 @@
+package com.lttbdd.ebook_app;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class Fragment2 extends Fragment {
+
+    View view;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_2, container, false);
+        return view;
+    }
+}
