f7c3151b8f0666fbd40c94e30776c98e0cdc625d
Huy <dodaihoc318@gmail.com>
2021-12-23 22:33:36 +0700

    finished

--- app/src/main/java/com/lttbdd/ebook_app/Fragment1.java
+++ app/src/main/java/com/lttbdd/ebook_app/Fragment1.java
@@ -19,7 +19,6 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class Fragment1 extends Fragment implements AdapterView.OnItemClickListener {
-
     Communicator communicator;
     ArrayAdapter adapter;
     ListView listView;
